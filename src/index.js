3915b533d5732b2bac04f70122ae7c1ee4c60d05
kks91 <kokyusik91@naver.com>
2023-03-27 21:34:55 +0900

    ♻️ 리팩토링한 코드

--- src/index.js
+++ src/index.js
@@ -4,21 +4,21 @@ import stopwatch from './stopwatch.js'
 class StopWatcher {
     $startButton
     $resetButton
-    stopWatch
+    stopWatchInstance
     $timer
     $startButtonLabel
     $resetButtonLabel
     $laps
-    $isStart = false
-    $_interval
+    isRunning = false
+    interval
     labCount
     lapTimeArray = []
-    maxLapTime = 0
-    minLabTime = 0
+    maxLapTime
+    minLabTime
     constructor(stopWatchInstance) {
         this.assignElement()
         this.handleDomEvent()
-        this.stopWatch = stopWatchInstance
+        this.stopWatchInstance = stopWatchInstance
         // this.labCount = stopWatch._lapCount
     }
 
@@ -38,7 +38,7 @@ class StopWatcher {
     handleDomEvent() {
         this.$startButton.addEventListener(
             'click',
-            this.handleInitTime.bind(this)
+            this.onClickStartBtn.bind(this)
         )
         this.$resetButton.addEventListener(
             'click',
@@ -47,85 +47,109 @@ class StopWatcher {
         window.addEventListener('keydown', this.handleKeyDown.bind(this))
     }
 
-    handleInitTime() {
-        if (!this.$isStart) {
-            this.stopWatch.start()
-            this.$isStart = true
-            this.$startButton.classList.remove('bg-green-600')
-            this.$startButton.classList.add('bg-red-600')
-            this.$startButtonLabel.innerText = '중단'
-            this.$resetButtonLabel.innerText = '랩'
-            this.$_interval = setInterval(() => {
-                this.$timer.innerText = this.calculateTime(
-                    this.stopWatch.centisecond
+    onClickStartBtn() {
+        if (!this.isRunning) {
+            this.stopWatchInstance.start()
+            this.interval = setInterval(() => {
+                this.handleUpdateTime(
+                    this.calculateTime(this.stopWatchInstance.centisecond)
                 )
             }, 10)
+
+            this.$startButtonLabel.innerText = '중단'
+            this.$resetButtonLabel.innerText = '랩'
         } else {
-            this.$isStart = false
-            this.$startButton.classList.remove('bg-red-600')
-            this.$startButton.classList.add('bg-green-600')
+            this.stopWatchInstance.pause()
+            clearInterval(this.interval)
             this.$startButtonLabel.innerText = '시작'
             this.$resetButtonLabel.innerText = '리셋'
-            this.stopWatch.pause()
-            clearInterval(this.$_interval)
         }
+        this.isRunning = !this.isRunning
+        this.toggleBtnStyle()
     }
 
     handleResetTime() {
         // 타이머가 가고 있는 상황 랩을 기록해야함.
         // this.labCount =
-        if (this.$isStart) {
-            const [labCount, lapTime] = this.stopWatch.createLap()
-            this.lapTimeArray.push(lapTime)
-            this.maxLapTime = Math.max(...this.lapTimeArray)
-            this.minLabTime = Math.min(...this.lapTimeArray)
-
-            // ul 태그 하위의 자식들중에
-            const template = `
-                    <li data-item=${lapTime} class="flex justify-between py-2 px-3 border-b-2">
-                        <span>랩 ${labCount}</span>
-                        <span>${this.calculateTime(
-                            this.stopWatch.centisecond
-                        )}</span>
-                    </li>`
-            this.$laps.insertAdjacentHTML('afterbegin', template)
-
-            const listArray = [...this.$laps.querySelectorAll('li')]
-            listArray
-                .filter((item) => item.dataset.item !== this.maxLapTime)
-                .forEach((item) => item.classList.remove('text-red-600'))
-
-            listArray
-                .find((item) => item.dataset.item == this.maxLapTime)
-                .classList.add('text-red-600')
-
-            listArray
-                .filter((item) => item.dataset.item !== this.minLabTime)
-                .forEach((item) => item.classList.remove('text-green-600'))
-            listArray
-                .find((item) => item.dataset.item == this.minLabTime)
-                .classList.add('text-green-600')
-
-            // 타이머가 멈춘 상황 시간을 리셋해야함.
-        } else {
-            this.stopWatch.reset()
-            this.$timer.innerText = this.calculateTime(
-                this.stopWatch.centisecond
+        if (this.isRunning) {
+            const [labCount, lapTime] = this.stopWatchInstance.createLap()
+            const $lapItem = document.createElement('li')
+            $lapItem.setAttribute('data-time', lapTime)
+            $lapItem.classList.add(
+                'flex',
+                'justify-between',
+                'py-2',
+                'px-3',
+                'border-b-2'
             )
-            this.lapTimeArray = []
+            $lapItem.innerHTML = `
+              <span>랩 ${labCount}</span>
+                  <span>${this.calculateTime(
+                      this.stopWatchInstance.centisecond
+                  )}</span>
+            `
+            this.$laps.prepend($lapItem)
+            // 첫 값은 minLab이다.
+            if (this.minLabTime === undefined) {
+                this.minLabTime = $lapItem
+
+                return
+            }
+            // 두 번째 눌렸을때는 maxLabTime이 undefined이다. minLabTime은 위에서 $lapㅑㅅ드
+            if (this.maxLapTime === undefined) {
+                if (lapTime < this.minLabTime.dataset.time) {
+                    this.maxLapTime = this.minLabTime
+                    this.minLabTime = $lapItem
+                } else {
+                    this.maxLapTime = $lapItem
+                }
+                this.minLabTime.classList.add('text-green-600')
+                this.maxLapTime.classList.add('text-red-600')
+                return
+            }
+
+            // 랩이 3개인 경우
+            if (lapTime < this.minLabTime.dataset.time) {
+                this.minLabTime.classList.remove('text-green-600')
+                this.minLabTime = $lapItem
+            } else if (lapTime > this.maxLapTime.dataset.time) {
+                this.maxLapTime.classList.remove('text-red-600')
+                this.maxLapTime = $lapItem
+            }
+
+            this.minLabTime.classList.add('text-green-600')
+            this.maxLapTime.classList.add('text-red-600')
+        } else {
+            this.stopWatchInstance.reset()
+            this.$timer.innerText = this.calculateTime(
+                this.stopWatchInstance.centisecond
+            )
+            this.maxLapTime = undefined
+            this.minLabTime = undefined
             while (this.$laps.firstChild) {
                 this.$laps.removeChild(this.$laps.lastChild)
             }
         }
     }
 
-    handleKeyDown(e) {
-        if (e.key === 's' || e.key === 'ㄴ') {
-            this.handleInitTime()
-        }
+    // 돔에 시간을 업데이트 해주는 값
+    handleUpdateTime(time) {
+        this.$timer.innerText = time
+    }
 
-        if (e.key === 'l' || e.key === 'ㅣ') {
-            this.handleResetTime()
+    toggleBtnStyle() {
+        this.$startButton.classList.toggle('bg-green-600')
+        this.$startButton.classList.toggle('bg-red-600')
+    }
+
+    handleKeyDown(e) {
+        switch (e.code) {
+            case 'KeyL':
+                this.handleResetTime()
+                break
+            case 'KeyS':
+                this.onClickStartBtn()
+                break
         }
     }
 
